a5ebe23a2af9ee994b69b6cf85fdbf9419cbf0d1
Locateme-GPS <gowripshenoy@gmail.com>
2025-05-09 15:48:10 +0530

    Login page base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landingpage from './LandingPage'
 import ToDo from './Components/Div'
-
-
+import Login from './Login';
 
 function App() {
 
@@ -12,6 +11,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Landingpage />}/>
             <Route path='/ToDo' element={<ToDo />}/>
+            <Route path='/Login' element={<Login />}/>
         </Routes>
     </Router>        
 
